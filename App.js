52b70385638808ec75aabe1b78af357777563e1c
DavDan1 <david-danielyan@hotmail.com>
2021-05-25 15:16:22 +0200

    small bugfixes

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ const App = () => {
         options ={() => ({
           headerBackTitle: 'Back?'
         })}
-        ></Stack.Screen>
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screen/SecondScreen.js
+++ screen/SecondScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const SecondScreen = (props) => {
+  const name = props.route.params.name
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Yooo!! {props.route.params.name}</Text>
